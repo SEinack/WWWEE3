1f0ab827424844ec419e1e0d7afdb73811d1a761
SEinack <7751481@gmail.com>
2026-06-09 09:27:53 +0300

    Update and rename практична 4 to практична4.cs

--- src/практична4.cs
+++ src/практична4.cs
@@ -11,7 +11,6 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
-
 class Result
 {
     /*
@@ -38,7 +37,6 @@ class Result
                 applesCount++;
             }
         }
-
         foreach (int orange in oranges)
         {
             int position = b + orange;
@@ -47,12 +45,10 @@ class Result
                 orangesCount++;
             }
         }
-
         Console.WriteLine(applesCount);
         Console.WriteLine(orangesCount);
     }
 }
-
 class Solution
 {
     public static void Main(string[] args)
@@ -60,18 +56,14 @@ class Solution
         string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
         int s = Convert.ToInt32(firstMultipleInput[0]);
         int t = Convert.ToInt32(firstMultipleInput[1]);
-
         string[] secondMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
         int a = Convert.ToInt32(secondMultipleInput[0]);
         int b = Convert.ToInt32(secondMultipleInput[1]);
-
         string[] thirdMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
         int m = Convert.ToInt32(thirdMultipleInput[0]);
         int n = Convert.ToInt32(thirdMultipleInput[1]);
-
         List<int> apples = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(applesTemp => Convert.ToInt32(applesTemp)).ToList();
         List<int> oranges = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(orangesTemp => Convert.ToInt32(orangesTemp)).ToList();
-
         Result.countApplesAndOranges(s, t, a, b, apples, oranges);
     }
 }
